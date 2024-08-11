246fec8ec22434517f3f6b38d0c9243e61f66361
Abhilekh Sarmah <64719469+abhilekhsarmah200@users.noreply.github.com>
2024-08-11 14:08:33 +0530

    Remove react.lazy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,8 @@ import '@/styles/globals.css';
 import '@/styles/custom.css';
 import '@/styles/navbar.css';
 import type { AppProps } from 'next/app';
-import { lazy } from 'react';
-const Navbar = lazy(() => import('../components/NavBar/Navbar'));
-const Footer = lazy(() => import('../components/Footer/Footer'));
+import Navbar from '../components/NavBar/Navbar';
+import Footer from '../components/Footer/Footer';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
